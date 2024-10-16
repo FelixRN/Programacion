14c59f04b67c501b362b37045d46f57a88988a29
unknown <alumnofp@DESKTOP-SV1H8HS.CALASANZSALAMANCA.local>
2024-10-16 08:39:51 +0200

    ptm

---
+++ VARIOS/Computers.java
@@ -0,0 +1,23 @@
+class Computers {
+    String cpuName;
+    String MotherBoardName;
+    int RamGB;
+    String GraphicCardName;
+    int PowerSupplyWs;
+}
+
+class ComplexComputer {
+    String cpuName;
+    String MotherBoardName;
+    int RamGB;
+    String GraphicCardName;
+    int powerSupplyWs;
+}
+
+class RamInfo{
+    String brandName; 
+    int clockTicks;
+    String memoryType;
+    int storageGb;
+
+}
